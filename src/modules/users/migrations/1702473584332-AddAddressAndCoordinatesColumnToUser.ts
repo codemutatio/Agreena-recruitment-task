f7f02097095b5f39d55fe5bbf57c19db26200cf3
Faith Omojola <faithomojola@gmail.com>
2023-12-13 18:40:03 +0200

    chore(users): update migration name to match file name

--- src/modules/users/migrations/1702473584332-AddAddressAndCoordinatesColumnToUser.ts
+++ src/modules/users/migrations/1702473584332-AddAddressAndCoordinatesColumnToUser.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddAddressAndCoordinatesColumnToUser1702131350276 implements MigrationInterface {
+export class AddAddressAndCoordinatesColumnToUser1702473584332 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             ALTER TABLE "user"
